b14ae8282bbc6a35117b996ae17de4d3f2143658
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-09 17:23:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,7 @@
-def countdown (x)
-  count = x
-  while x < 10
-    puts "#{counter} SECOND(S)!"
-    x -=1
-  end
-  "HAPPY NEW YEAR!"
+def countdown(x)
+    while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    end
+  puts 'HAPPY NEW YEAR!'
 end
